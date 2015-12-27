e87c983c7f4c6f76cd728ea0e8c89a8ab24a0b92
zpx <DEFEC8ED@users.noreply.github.com>
2015-12-27 22:20:14 +0100

    Update core.c

--- core.c
+++ core.c
@@ -1,35 +1,8 @@
-// Functions comes here.
-
+#include "core.h"
 #include "colors.h"
 
-#define MAX 64
-#define MAXLINE 2048
-#define FILESIZE 1024
-
-FILE *file; // original
-FILE *output; // temp output
-
-const char database[] = "students.txt";
-
-char add_new_student_menu(void);
-char adding_ch(void);
-char remove_student(void);
-char list_all_students(void);
-char find_student(unsigned char *keyword);
-void cleantime(void);
-
-typedef struct students
+void cleantime()
 {
-	char firstname[MAX];
-	char lastname[MAX];
-	char email[MAX];
-	char tel[15];
-
-} Students;
-
-Students student[128];
-
-void cleantime() {
     while(getchar() != '\n');
 }
 
